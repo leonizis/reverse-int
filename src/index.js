22cb4f5531fc741973338b8419beea3a61e855d4
leonizis <leonidu36@gmail.com>
2023-01-04 19:09:21 +0300

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function reverse (n) {
+    n = Math.abs(n);
+    let nTxt = String(n);
+    let tTxt = nTxt;
+    let numberTxt = '';
+    for(let i = 0; i < nTxt.length; i++){
+        let a = n % 10;
+        n = String(n);
+        n = n.slice(0, -1);
+        n = Number(n);
+        numberTxt = numberTxt + String(a);
+
+  }
   
-}
+
+  return numberTxt;
+};
+
+module.exports(-367);
+
+
+
+
+
